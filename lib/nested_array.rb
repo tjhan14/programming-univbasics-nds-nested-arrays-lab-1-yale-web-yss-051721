cee6aea89209b392b1f037380cef2d637a6f9fca
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-02 03:49:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -45,7 +45,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   
-  matrix_lookup(assembled_matrix, 1, 1)
+  puts matrix_lookup(sorted_matrix, 1, 1)
   
 end
 
@@ -54,6 +54,6 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
-  matrix_update(assembled_matrix, 1, 1, "Mangos")
+  puts matrix_update(sorted_matrix, 1, 1, "Mangos")
   
 end
